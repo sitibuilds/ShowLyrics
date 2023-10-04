31d262b6b3cd42d70515a17f2e0c9d795e0cfa65
sitibuilds <samtsiaw+sitibuilds@gmail.com>
2023-10-04 01:00:52 -0400

    model.lyric: add currently playing song retriever

--- src/models/lyrics/api.py
+++ src/models/lyrics/api.py
@@ -1,19 +1,39 @@
 import syncedlyrics
+import spotipy
+from data_types import TrackDetails
+from dotenv import load_dotenv
+import os
+
+
+# TODO: Allow caller to specify these values; Remove load_dotenv from this module
+SCRIPT_PATH = os.path.abspath(os.path.realpath(__file__))
+dotenv_path = os.path.abspath(os.path.join(SCRIPT_PATH, "..", "..", "..", "..", ".env"))
+load_dotenv(dotenv_path)
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+
+SPOTIFY_SCOPE_READ_PLAYBACK_STATE = "user-read-playback-state"
+SPOTIFY_SCOPE_MODIFY_PLAYBACK_STATE = "user-modify-playback-state"
+SPOTIFY_SCOPE_READ_CURRENTLY_PLAYING = "user-read-currently-playing"
+SCOPES = [
+    SPOTIFY_SCOPE_READ_PLAYBACK_STATE,
+    SPOTIFY_SCOPE_MODIFY_PLAYBACK_STATE,
+    SPOTIFY_SCOPE_READ_CURRENTLY_PLAYING,
+]
+
 
 def get_lrc_lyrics(track_name, artist_name):
     # type: (str, str) -> dict[str, str | None]
     """
-    Inspired by the `syncedlyrics.search` function
-
     Retrieves the lyric for the track specified in the search term in LRC format.
-    
+
     search_term: `[TRACK_NAME] [ARTIST_NAME]`
     """
-    
-    transform_str = lambda s : s.strip().replace(" ", "_")
-    search_term = transform_str(track_name) + " " + transform_str(artist_name)
 
-    print(search_term)
+    transform_str = lambda s: s.strip().replace(" ", "_")
+    search_term = transform_str(track_name) + " " + transform_str(artist_name)
 
     _providers = [
         syncedlyrics.Lrclib(),
@@ -28,7 +48,7 @@ def get_lrc_lyrics(track_name, artist_name):
     }
 
     for provider in _providers:
-        
+
         lrc_lyrics = provider.get_lrc(search_term)
 
         if syncedlyrics.is_lrc_valid(lrc_lyrics):
@@ -38,3 +58,92 @@ def get_lrc_lyrics(track_name, artist_name):
 
     return result
 
+
+def spotify_acquire_token_info(cached_token_info=None):
+    # type: (dict | None) -> dict
+    """
+    Retrieve token info for authenticated user
+
+    This function will take the user to the Spotify authentication page on their web browser.
+    The token info is not stored locally, within this function
+
+    """
+
+    cache_handler = spotipy.cache_handler.MemoryCacheHandler(cached_token_info)
+    oauth = spotipy.oauth2.SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope=SCOPES,
+        cache_handler=cache_handler,
+    )
+
+    # Validate cached token. If expired, refresh token info
+    token_info = oauth.validate_token(cache_handler.get_cached_token())
+
+    if token_info is None:  # No token exists
+
+        token_info = oauth.get_access_token(None, as_dict=True)
+
+        # Note: This is only for future versions of spotipy which may not support as_dict in get_access_token
+        if isinstance(token_info, (str,)):
+            return cache_handler.get_cached_token()
+
+        return token_info
+
+    else:
+        return token_info
+
+
+def get_currently_playing_song(cached_token_info=None):
+
+    token_info = spotify_acquire_token_info(cached_token_info)
+
+    token = token_info["access_token"]
+
+    try:
+        spotify_obj = spotipy.Spotify(auth=token)
+
+        track = spotify_obj.currently_playing()
+    except:
+        # TODO: Log error
+        return None
+
+    if track is not None:
+        track_type = track["currently_playing_type"]
+        meta = {
+            "actions": track["actions"],
+        }
+
+        item = track["item"]
+        name = ""
+        artists = []
+        is_playing = track["is_playing"]
+        progress_ms = track["progress_ms"] if track["progress_ms"] else -1
+        duration_ms = -1
+        popularity = -1
+
+        if item:
+            name = item["name"]
+            popularity = item["popularity"] / 100
+            duration_ms = item["duration_ms"]
+            artists = list(map(lambda x: x["name"], item["artists"]))
+
+        return TrackDetails(
+            name=name,
+            artists=artists,
+            is_playing=is_playing,
+            ms_remote=progress_ms,
+            duration_ms=duration_ms,
+            popularity=popularity,
+            meta=meta,
+            track_type=track_type,
+        )
+
+    return track
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    pprint(get_currently_playing_song())
--- src/models/lyrics/data_types.py
+++ src/models/lyrics/data_types.py
@@ -1,5 +1,5 @@
-from typing import TypedDict, TypeAlias
 import enum
+from dataclasses import dataclass, asdict, field
 
 class LyricSource(enum.Enum):
     Musixmatch = enum.auto()
@@ -7,21 +7,32 @@ class LyricSource(enum.Enum):
     MegaLobiz = enum.auto()
     NetEase = enum.auto()
 
+
 class LRCEntry:
-    __slots__ = ('lyric', 'time')
+    __slots__ = ("lyric", "time")
+
+    def __init__(self, lyric="", time=None) -> None:
+        # type: (str, None | LyricTime) -> None
+        self.lyric = lyric
+        self.time = time
 
     def __str__(self):
         return str({"lyric": self.lyric, "time": str(self.time)})
-    
+
     def __repr__(self):
         return f"LRCEntry(time={str(self.time)}, lyric={self.lyric})"
 
+
 class LyricTime:
-    __slots__ = ('mins', 'secs', 'ms')
+    """
+    A class for representing playback time in mins,secs, and milliseconds format
+    """
+
+    __slots__ = ("mins", "secs", "ms", "__ms_cached")
 
     def __init__(self, mins, secs, ms):
         # type: (int, int, int) -> None
-        
+
         self.validate_time(mins, secs, ms)
         self.mins = mins
         self.secs = secs
@@ -29,29 +40,29 @@ class LyricTime:
 
     def __str__(self):
         return f"{self.mins}:{self.secs}.{self.ms}"
-    
+
     def __repr__(self):
         return f"Time(mins={self.mins}, secs={self.secs}, ms={self.ms})"
-    
+
     def __add__(self, other):
         # type: (LyricTime) -> LyricTime
         if isinstance(other, LyricTime):
             ms = self.to_milliseconds() + other.to_milliseconds()
             return LyricTime.convert_from_milliseconds(ms)
         raise TypeError("Cannot add to object of type %s" % type(other))
-    
+
     def __sub__(self, other):
         # type: (LyricTime) -> LyricTime
         if isinstance(other, LyricTime):
             ms = abs(self.to_milliseconds() + other.to_milliseconds())
             return LyricTime.convert_from_milliseconds(ms)
         raise TypeError("Cannot subtract from object of type %s" % type(other))
-    
+
     @staticmethod
     def strptime(time_str, format="%M:%S.%m"):
         # type: (str, str) -> LyricTime
         """Pass LyricTime from a given string.
-        
+
         Placeholders are denoted by a '%' char and another character specifying the semantics of the time it represents.
 
         Accepted placeholders: %M - Minute, %S - Second, %m - millisecond, %% - Match the char '%'
@@ -64,10 +75,10 @@ class LyricTime:
         valid_placeholders = ("%M", "%S", "%m", "%%")
 
         mins, secs, ms = -1, -1, -1
-        
-        time_str_idx = 0 # `time_str` has been validated upto this index as matching the provided format
-        format_idx = 0 # `format` has been validated upto this index
-        
+
+        time_str_idx = 0  # `time_str` has been validated upto this index as matching the provided format
+        format_idx = 0  # `format` has been validated upto this index
+
         def find_non_numeric_idx(string, start=0):
             # type: (str, int) -> int
 
@@ -76,14 +87,16 @@ class LyricTime:
                 if c not in nums:
                     return i
             return -1
-        
+
         while time_str_idx < len(time_str) and format_idx < len(format):
             t, f = time_str[time_str_idx], format[format_idx]
-            
+
             if f != "%":
                 if t != f:
-                    raise ValueError(f"Time string '{time_str}' does not match format '{format}'")
-                else:                    
+                    raise ValueError(
+                        f"Time string '{time_str}' does not match format '{format}'"
+                    )
+                else:
                     time_str_idx += 1
                     format_idx += 1
             else:
@@ -92,8 +105,10 @@ class LyricTime:
                     placeholder = format[format_idx : format_idx + 2]
 
                     if placeholder not in valid_placeholders:
-                        raise ValueError("Invalid format. Unknown placeholder: %s" % placeholder)
-                    
+                        raise ValueError(
+                            "Invalid format. Unknown placeholder: %s" % placeholder
+                        )
+
                     if placeholder == "%%":
                         if time_str_idx < len(time_str):
                             if time_str[time_str_idx] == "%":
@@ -101,81 +116,142 @@ class LyricTime:
                                 format_idx += 2
                                 continue
 
-                        raise ValueError("Time string '{time_str}' does not match format '{format}'")
+                        raise ValueError(
+                            "Time string '{time_str}' does not match format '{format}'"
+                        )
 
                     # Check placeholder value in time string
                     i = find_non_numeric_idx(time_str, time_str_idx)
                     if i == -1:
                         value_str = time_str[time_str_idx:]
                     else:
-                        value_str = time_str[time_str_idx: i]
+                        value_str = time_str[time_str_idx:i]
 
                     try:
                         value = int(value_str)
 
                         if placeholder == "%M":
-                            if mins == -1: mins = value
-                            else: raise ValueError(f"Invalid format. Multiple instances of placeholder '{placeholder}' not allowed")
-                        
+                            if mins == -1:
+                                mins = value
+                            else:
+                                raise ValueError(
+                                    f"Invalid format. Multiple instances of placeholder '{placeholder}' not allowed"
+                                )
+
                         elif placeholder == "%m":
-                            if ms == -1: ms = value
-                            else: raise ValueError(f"Invalid format. Multiple instances of placeholder '{placeholder}' not allowed")
-                        
+                            if ms == -1:
+                                ms = value
+                            else:
+                                raise ValueError(
+                                    f"Invalid format. Multiple instances of placeholder '{placeholder}' not allowed"
+                                )
+
                         elif placeholder == "%S":
-                            if secs == -1: secs = value
-                            else: raise ValueError(f"Invalid format. Multiple instances of placeholder '{placeholder}' not allowed")
-                        
+                            if secs == -1:
+                                secs = value
+                            else:
+                                raise ValueError(
+                                    f"Invalid format. Multiple instances of placeholder '{placeholder}' not allowed"
+                                )
+
                         else:
                             raise ValueError(f"Invalid format")
-                        
+
                         time_str_idx = i if i != -1 else len(time_str)
                         format_idx += 2
-                        
+
                     except ValueError:
-                        raise ValueError(f"Time string '{time_str}' does not match format '{format}'")
+                        raise ValueError(
+                            f"Time string '{time_str}' does not match format '{format}'"
+                        )
                 else:
-                    raise ValueError("Invalid format. '%' must be followed by another character")
+                    raise ValueError(
+                        "Invalid format. '%' must be followed by another character"
+                    )
 
         if time_str_idx >= len(time_str) and format_idx >= len(format):
-            if mins == -1: mins = 0
-            if secs == -1: secs = 0
-            if ms == -1: ms = 0
+            if mins == -1:
+                mins = 0
+            if secs == -1:
+                secs = 0
+            if ms == -1:
+                ms = 0
         else:
-            raise ValueError(f"Time string '{time_str}' does not match format '{format}'")
+            raise ValueError(
+                f"Time string '{time_str}' does not match format '{format}'"
+            )
 
         return LyricTime(mins, secs, ms)
-    
+
     @staticmethod
     def validate_time(mins, secs, ms):
 
         assert 0 <= mins <= 59
         assert 0 <= secs <= 59
         assert 0 <= ms <= 999
-    
+
     def to_milliseconds(self):
         # type: () -> int
-        
-        ms = (self.mins*60*1000) + (self.secs*1000) + self.ms
+        if self.__ms_cached is not None:
+            return self.__ms_cached
+        self.__ms_cached = ms = (self.mins * 60 * 1000) + (self.secs * 1000) + self.ms
         return ms
 
     @staticmethod
     def convert_from_milliseconds(ms):
         # type: (int) -> LyricTime
-        
-        _s = int(ms/1000)
-        secs = _s%60
-        mins = (_s//60)%60
-        ms = int(ms)%1000
 
+        _s = int(ms / 1000)
+        secs = _s % 60
+        mins = (_s // 60) % 60
+        ms = int(ms) % 1000
 
         return LyricTime(mins, secs, ms)
-    
+
     def __eq__(self, __value):
         # type: (object) -> bool
         if isinstance(__value, LyricTime):
-            return __value.mins == self.mins and __value.secs == self.secs and __value.ms == self.ms
+            return (
+                __value.mins == self.mins
+                and __value.secs == self.secs
+                and __value.ms == self.ms
+            )
         return False
+
+    def __lt__(self, __value):
+        if isinstance(__value, LyricTime):
+            return self.to_milliseconds() < __value.to_milliseconds()
+
+        elif isinstance(__value, int):
+            return self.to_milliseconds() < __value
+
+        raise TypeError(
+            f"Cannot compare object of type {type(self)} to object of type {type(__value)}"
+        )
+
+@dataclass
+class CustomDataClass:
+
+    def to_dict(self):
+        return asdict(self)
+
+@dataclass(kw_only=True, repr=True, init=True)
+class TrackDetails(CustomDataClass):
+    name: str # Name of the track
+    artists: list[str] # List of artists who performed the track
+    is_playing: bool
+    track_type: str # Can be one of 'track', 'episode', 'ad', or 'unknown'
+    duration_ms: int # Track duration
+    ms_remote: int = -1  # Remote progress in milliseconds if song is currently playing. (-1 if not playing)
+    popularity: float = -1 # Track popularity
+    meta: dict = field(default_factory=dict) # Meta information which provides some context for the currently playing track
+        
+    def __eq__(self, val):
+        if isinstance(val, TrackDetails):
+            return val.name == self.name and self.track_type == val.track_type and val.artists == self.artists
+        raise TypeError("Cannot compare object of type %s" % type(val) + " to obejct of type %s" % type(self))
     
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     t = LyricTime.strptime("50:20.09", "%M:%S.%m")
